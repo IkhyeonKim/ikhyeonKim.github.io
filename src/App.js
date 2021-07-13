a044b4fe7e543c25e42626bda6191165fab2f053
ikhyeon.kim <ikhyeonkim@Ikhyeons-MacBook-Air.local>
2021-07-13 22:45:15 +0900

    Ball bouncing #1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import BallPage from "./components/pages/ball-page";
 import MainPage from "./components/pages/main-page";
 import { LayoutProvider } from "./contexts/layout-store";
+import GlobalStyle from "./styles/global-style";
 
 function App() {
   return (
     <LayoutProvider>
-      <MainPage></MainPage>;
+      <GlobalStyle />
+      <BallPage></BallPage>
+      {/* <MainPage></MainPage> */}
     </LayoutProvider>
   );
 }
