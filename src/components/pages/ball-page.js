e696fd8f9aebd62786e4e869dbdaad9c3eb6976e
ikhyeon.kim <ikhyeonkim@Ikhyeons-MacBook-Air.local>
2021-07-14 23:42:04 +0900

    Let's bounce

--- src/components/pages/ball-page.js
+++ src/components/pages/ball-page.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import BallBounceStyle from "../../styles/ball-collision-bouce-style";
 
 class Ball {
@@ -23,7 +23,9 @@ class Ball {
   }
 }
 
-const BallPage = () => {
+const BallPage = (props) => {
+  const { ...rest } = props;
+
   const canvasRef = useRef();
   const requestRef = useRef();
   const [ctx, setCtx] = useState();
@@ -36,12 +38,42 @@ const BallPage = () => {
     requestRef.current = requestAnimationFrame(animate);
   };
 
-  const draw = (ctx) => {
-    ctx.fillStyle = "#000";
+  const bounceWindow = useCallback((canvasWidth, canvasHeight, x, y, speed, radius, vx, vy) => {
+    const minX = radius;
+    const minY = radius;
+    const maxX = canvasWidth - radius;
+    const maxY = canvasHeight - radius;
+
+    let newX = x;
+    let newY = y;
+    let _vx = vx;
+    let _vy = vy;
+
+    if (x <= minX || x >= maxX) {
+      _vx *= -1;
+      newX += _vx;
+    } else if (y <= minY || y >= maxY) {
+      _vy *= -1;
+      newY += _vy;
+    } else {
+      newX += _vx;
+      newY += _vy;
+    }
+
+    return { x: newX, y: newY, vx: _vx, vy: _vy };
+  }, []);
+
+  const drawBall = useCallback((ctx, frameCount, x, y, radius) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    // console.log({ ctx, frameCount, x, y, radius });
+
+    ctx.fillStyle = "#fdd700";
     ctx.beginPath();
-    ctx.arc(50, 100, 20, 0, 2 * Math.PI);
+    ctx.arc(x, y, radius, 0, 2 * Math.PI); // x,y,radius
+    // ctx.arc(x, y, radius * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI); // x,y,radius
     ctx.fill();
-  };
+  }, []);
 
   //   useEffect(() => {
   //     requestRef.current = requestAnimationFrame(animate);
@@ -63,13 +95,32 @@ const BallPage = () => {
     setCtx(canvasRef.current.getContext("2d"));
     const context = canvasRef.current.getContext("2d");
 
+    console.log(canvasRef.current.width);
+    let speed = 5;
+    const radius = 50;
+
+    const diameter = radius * 2;
+
+    let x = radius + Math.random() * (canvasRef.current.width - diameter);
+    let y = radius + Math.random() * (canvasRef.current.height - diameter);
+    let vx = speed;
+    let vy = speed;
+
     let frameCount = 0;
     let animationFrameId;
 
     //Our draw came here
     const render = () => {
       frameCount++;
-      draw(context, frameCount);
+
+      const newXY = bounceWindow(canvasRef.current.width, canvasRef.current.height, x, y, speed, radius, vx, vy);
+      x = newXY.x;
+      y = newXY.y;
+      vx = newXY.vx;
+      vy = newXY.vy;
+      //   speed = newXY.speed;
+
+      drawBall(context, frameCount, x, y, radius);
       animationFrameId = window.requestAnimationFrame(render);
     };
     render();
@@ -77,11 +128,11 @@ const BallPage = () => {
     return () => {
       window.cancelAnimationFrame(animationFrameId);
     };
-  }, []);
+  }, [bounceWindow, drawBall]);
 
   return (
     <BallBounceStyle>
-      <canvas width={canvasWidth} height={canvasHeight} ref={canvasRef}></canvas>
+      <canvas width={canvasWidth} height={canvasHeight} ref={canvasRef} {...rest}></canvas>
     </BallBounceStyle>
   );
 };
