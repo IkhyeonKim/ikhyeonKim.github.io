09a0615100f65e8978abcbde8e763799e17b1516
ikhyeon.kim <ikhyeonkim@Ikhyeons-MacBook-Air.local>
2021-07-17 17:05:13 +0900

    Add block

--- src/components/pages/ball-page.js
+++ src/components/pages/ball-page.js
@@ -23,6 +23,8 @@ class Ball {
   }
 }
 
+const radius = 50;
+
 const BallPage = (props) => {
   const { ...rest } = props;
 
@@ -63,15 +65,79 @@ const BallPage = (props) => {
     return { x: newX, y: newY, vx: _vx, vy: _vy };
   }, []);
 
-  const drawBall = useCallback((ctx, frameCount, x, y, radius) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  const bounceBlock = useCallback((block, circleX, circleY, vx, vy) => {
+    const { width, height, x, y } = block;
+    const minX = x - radius;
+    const minY = y - radius;
+    const maxX = x + width + radius;
+    const maxY = y + height + radius;
 
+    let newX = circleX;
+    let newY = circleY;
+    let _vx = vx;
+    let _vy = vy;
+
+    if (circleX > minX && circleX < maxX && circleY > minY && circleY < maxY) {
+      const x1 = Math.abs(minX - circleX);
+      const x2 = Math.abs(circleX - maxX);
+      const y1 = Math.abs(minY - circleY);
+      const y2 = Math.abs(circleY - maxY);
+
+      const min1 = Math.min(x1, x2);
+      const min2 = Math.min(y1, y2);
+      const min = Math.min(min1, min2);
+
+      if (min === min1) {
+        // 엑스 축으로 튕기는가
+        _vx *= -1;
+        newX += _vx;
+      } else if (min === min2) {
+        // y축으로 튕기는가
+        _vy *= -1;
+        newY += _vy;
+      }
+    }
+    return { x: newX, y: newY, vx: _vx, vy: _vy };
+  }, []);
+
+  const drawBall = useCallback((ctx, frameCount, x, y, radius) => {
     // console.log({ ctx, frameCount, x, y, radius });
 
     ctx.fillStyle = "#fdd700";
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, 2 * Math.PI); // x,y,radius
     // ctx.arc(x, y, radius * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI); // x,y,radius
+    ctx.fill();
+  }, []);
+
+  const drawBlock = useCallback((ctx, width, height, x, y) => {
+    const xGap = 70;
+    const yGap = 70;
+
+    const maxX = width + x;
+    const maxY = height + y;
+
+    ctx.fillStyle = "#ff384e";
+    ctx.beginPath();
+    ctx.rect(x, y, width, height);
+    ctx.fill();
+
+    // bottom shadow
+    ctx.fillStyle = "#190f3a";
+    ctx.beginPath();
+    ctx.moveTo(maxX, maxY);
+    ctx.lineTo(maxX - xGap, maxY + yGap);
+    ctx.lineTo(x - xGap, maxY + yGap);
+    ctx.lineTo(x, maxY);
+    ctx.fill();
+
+    // side shadow
+    ctx.fillStyle = "#9d0919";
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x, maxY);
+    ctx.lineTo(x - xGap, maxY + yGap);
+    ctx.lineTo(x - xGap, maxY + yGap - height);
     ctx.fill();
   }, []);
 
@@ -96,8 +162,7 @@ const BallPage = (props) => {
     const context = canvasRef.current.getContext("2d");
 
     console.log(canvasRef.current.width);
-    let speed = 5;
-    const radius = 50;
+    let speed = 10;
 
     const diameter = radius * 2;
 
@@ -110,8 +175,10 @@ const BallPage = (props) => {
     let animationFrameId;
 
     //Our draw came here
+
     const render = () => {
       frameCount++;
+      context.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
 
       const newXY = bounceWindow(canvasRef.current.width, canvasRef.current.height, x, y, speed, radius, vx, vy);
       x = newXY.x;
@@ -120,7 +187,15 @@ const BallPage = (props) => {
       vy = newXY.vy;
       //   speed = newXY.speed;
 
+      drawBlock(context, 600, 20, 170, 230);
       drawBall(context, frameCount, x, y, radius);
+
+      const bouncedXY = bounceBlock({ width: 600, height: 20, x: 170, y: 230 }, x, y, vx, vy);
+      x = bouncedXY.x;
+      y = bouncedXY.y;
+      vx = bouncedXY.vx;
+      vy = bouncedXY.vy;
+
       animationFrameId = window.requestAnimationFrame(render);
     };
     render();
@@ -128,7 +203,7 @@ const BallPage = (props) => {
     return () => {
       window.cancelAnimationFrame(animationFrameId);
     };
-  }, [bounceWindow, drawBall]);
+  }, [bounceWindow, bounceBlock, drawBall, drawBlock]);
 
   return (
     <BallBounceStyle>
